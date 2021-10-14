ad2625088a90a50436e834469c26988482af5b57
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2021-10-14 18:45:52 -0300

    Add core output lines

--- README.md
+++ README.md
@@ -1,15 +1,40 @@
 # ABOUT
 Very simple program that parses the first line from /proc/stat file.
+This is a workaround for situations in which we need to get a better understanding about each CPU time and we don't have the output of "top" command (like, when we have only the data from sosreport available).
 
 # RUNNING
 * without parameters it will read the /proc/stat from the current system
 ~~~
 $ go run main.go
-%Cpu(s): 9.6 us, 2.7 sy, 0.1 ni, 86.7 id, 0.0 wa, 0.6 hi, 0.3 si, 0.0 st
+%Cpu(s): 9.1 us, 2.6 sy, 0.1 ni, 87.3 id, 0.0 wa, 0.6 hi, 0.3 si, 0.0 st
+%cpu0: 6.9 us, 1.9 sy, 0.0 ni, 90.3 id, 0.1 wa, 0.4 hi, 0.4 si, 0.0 st
+%cpu1: 9.7 us, 2.7 sy, 0.1 ni, 86.4 id, 0.0 wa, 0.7 hi, 0.4 si, 0.0 st
+%cpu2: 9.8 us, 2.7 sy, 0.1 ni, 86.6 id, 0.0 wa, 0.6 hi, 0.2 si, 0.0 st
+%cpu3: 9.7 us, 2.8 sy, 0.1 ni, 86.5 id, 0.1 wa, 0.6 hi, 0.3 si, 0.0 st
+%cpu4: 9.6 us, 2.7 sy, 0.1 ni, 86.8 id, 0.0 wa, 0.6 hi, 0.2 si, 0.0 st
+%cpu5: 9.6 us, 2.7 sy, 0.1 ni, 86.8 id, 0.0 wa, 0.6 hi, 0.2 si, 0.0 st
+%cpu6: 9.1 us, 2.8 sy, 0.1 ni, 86.8 id, 0.0 wa, 0.9 hi, 0.3 si, 0.0 st
+%cpu7: 9.6 us, 2.8 sy, 0.1 ni, 86.6 id, 0.0 wa, 0.6 hi, 0.2 si, 0.0 st
 ~~~
 
-* it is also possible to pass a stat file gathered from another system. For example:
+* it is also possible to pass a stat file gathered from another system (through sosreport, for example):
 ~~~
 $ go run main sosreport-hostabc-12345/proc/stat
-%Cpu(s): 5.4 us, 1.7 sy, 0.0 ni, 74.4 id, 17.8 wa, 0.3 hi, 0.3 si, 0.0 st
+%Cpu(s): 5.2 us, 1.4 sy, 0.0 ni, 91.8 id, 1.0 wa, 0.3 hi, 0.3 si, 0.0 st
+%cpu0: 5.4 us, 1.6 sy, 0.0 ni, 91.8 id, 0.8 wa, 0.3 hi, 0.1 si, 0.0 st
+%cpu1: 6.1 us, 1.8 sy, 0.0 ni, 90.5 id, 0.8 wa, 0.4 hi, 0.5 si, 0.0 st
+%cpu2: 5.8 us, 1.7 sy, 0.0 ni, 90.8 id, 1.2 wa, 0.3 hi, 0.1 si, 0.0 st
+%cpu3: 5.7 us, 1.6 sy, 0.0 ni, 91.5 id, 0.8 wa, 0.3 hi, 0.1 si, 0.0 st
+%cpu4: 5.4 us, 1.5 sy, 0.0 ni, 91.8 id, 0.9 wa, 0.4 hi, 0.1 si, 0.0 st
+%cpu5: 5.4 us, 1.5 sy, 0.0 ni, 91.8 id, 0.7 wa, 0.3 hi, 0.3 si, 0.0 st
+%cpu6: 5.5 us, 1.5 sy, 0.0 ni, 91.4 id, 0.8 wa, 0.3 hi, 0.4 si, 0.0 st
+%cpu7: 6.4 us, 1.7 sy, 0.0 ni, 90.0 id, 1.2 wa, 0.3 hi, 0.3 si, 0.0 st
+%cpu8: 4.9 us, 1.6 sy, 0.0 ni, 92.1 id, 1.1 wa, 0.2 hi, 0.1 si, 0.0 st
+%cpu9: 4.2 us, 1.4 sy, 0.0 ni, 91.9 id, 1.4 wa, 0.3 hi, 0.8 si, 0.0 st
+%cpu10: 4.9 us, 1.2 sy, 0.0 ni, 92.6 id, 1.0 wa, 0.2 hi, 0.1 si, 0.0 st
+%cpu11: 4.6 us, 1.4 sy, 0.0 ni, 92.2 id, 1.0 wa, 0.2 hi, 0.5 si, 0.0 st
+%cpu12: 5.4 us, 1.2 sy, 0.0 ni, 91.8 id, 1.0 wa, 0.2 hi, 0.5 si, 0.0 st
+%cpu13: 4.2 us, 1.0 sy, 0.0 ni, 93.6 id, 0.9 wa, 0.2 hi, 0.1 si, 0.0 st
+%cpu14: 4.2 us, 1.2 sy, 0.0 ni, 93.1 id, 0.9 wa, 0.2 hi, 0.5 si, 0.0 st
+%cpu15: 4.8 us, 1.3 sy, 0.0 ni, 92.4 id, 1.2 wa, 0.2 hi, 0.1 si, 0.0 st
 ~~~
--- main.go
+++ main.go
@@ -14,7 +14,9 @@ func main() {
 	var statFile *os.File
 	var err error
 
-	if len(os.Args) > 1 {
+	// If a file is passed as argument it will be used
+	// if not, use the current system /proc/stat
+	if len(os.Args) == 2 {
 		statFile, err = os.Open(os.Args[1])
 	} else {
 		statFile, err = os.Open("/proc/stat")
@@ -26,37 +28,46 @@ func main() {
 
 	defer statFile.Close()
 
-	r := regexp.MustCompile(`^cpu[\d]*\s+(?P<user>\d+)\s+(?P<nice>\d+)\s+(?P<system>\d+)\s+(?P<idle>\d+)\s+(?P<iowait>\d+)\s+(?P<irq>\d+)\s+(?P<softirq>\d+)\s+(?P<steal>\d+)\s+(?P<guest>\d+)\s+(?P<guestNice>\d+)$`)
+	// regular expression for the stat file cpu time line format
+	r := regexp.MustCompile(`^(?P<core>cpu[\d]*)\s+(?P<user>\d+)\s+(?P<nice>\d+)\s+(?P<system>\d+)\s+(?P<idle>\d+)\s+(?P<iowait>\d+)\s+(?P<irq>\d+)\s+(?P<softirq>\d+)\s+(?P<steal>\d+)\s+(?P<guest>\d+)\s+(?P<guestNice>\d+)$`)
 
 	scanner := bufio.NewScanner(statFile)
 
 	for scanner.Scan() {
-		match := r.FindStringSubmatch(scanner.Text())
 
-		cpuUser, _ := strconv.Atoi(match[1])
-		cpuNice, _ := strconv.Atoi(match[2])
-		cpuSystem, _ := strconv.Atoi(match[3])
-		cpuIdle, _ := strconv.Atoi(match[4])
-		cpuIOWait, _ := strconv.Atoi(match[5])
-		cpuIRQ, _ := strconv.Atoi(match[6])
-		cpuSoftIRQ, _ := strconv.Atoi(match[7])
-		cpuSteal, _ := strconv.Atoi(match[8])
-		cpuGuest, _ := strconv.Atoi(match[9])
-		cpuGuestNice, _ := strconv.Atoi(match[10])
-		cpuTotal := cpuUser + cpuNice + cpuSystem + cpuIdle + cpuIOWait + cpuIRQ + cpuSoftIRQ + cpuSteal + cpuGuest + cpuGuestNice
+		// if didn't match the regex, abort execution
+		if match := r.FindStringSubmatch(scanner.Text()); len(match) > 0 {
+			cpuCore := match[1]
+			cpuUser, _ := strconv.Atoi(match[2])
+			cpuNice, _ := strconv.Atoi(match[3])
+			cpuSystem, _ := strconv.Atoi(match[4])
+			cpuIdle, _ := strconv.Atoi(match[5])
+			cpuIOWait, _ := strconv.Atoi(match[6])
+			cpuIRQ, _ := strconv.Atoi(match[7])
+			cpuSoftIRQ, _ := strconv.Atoi(match[8])
+			cpuSteal, _ := strconv.Atoi(match[9])
+			cpuGuest, _ := strconv.Atoi(match[10])
+			cpuGuestNice, _ := strconv.Atoi(match[11])
+			cpuTotal := cpuUser + cpuNice + cpuSystem + cpuIdle + cpuIOWait + cpuIRQ + cpuSoftIRQ + cpuSteal + cpuGuest + cpuGuestNice
 
-		fmt.Print("%Cpu(s): ")
-		fmt.Printf("%.1f us, ", float32(cpuUser)/float32(cpuTotal)*100)
-		fmt.Printf("%.1f sy, ", float32(cpuSystem)/float32(cpuTotal)*100)
-		fmt.Printf("%.1f ni, ", float32(cpuNice)/float32(cpuTotal)*100)
-		fmt.Printf("%.1f id, ", float32(cpuIdle)/float32(cpuTotal)*100)
-		fmt.Printf("%.1f wa, ", float32(cpuIOWait)/float32(cpuTotal)*100)
-		fmt.Printf("%.1f hi, ", float32(cpuIRQ)/float32(cpuTotal)*100)
-		fmt.Printf("%.1f si, ", float32(cpuSoftIRQ)/float32(cpuTotal)*100)
-		fmt.Printf("%.1f st\n", float32(cpuSteal)/float32(cpuTotal)*100)
-		//fmt.Printf("guest: %f%% ", float32(cpuGuest)/float32(cpuTotal))
-		//fmt.Printf("guestNice: %f%%\n", float32(cpuGuestNice)/float32(cpuTotal))
-		break
+			if cpuCore == "cpu" {
+				fmt.Print("%Cpu(s): ")
+			} else {
+				fmt.Printf("%%%v: ", cpuCore)
+			}
+
+			fmt.Printf("%.1f us, ", float32(cpuUser)/float32(cpuTotal)*100)
+			fmt.Printf("%.1f sy, ", float32(cpuSystem)/float32(cpuTotal)*100)
+			fmt.Printf("%.1f ni, ", float32(cpuNice)/float32(cpuTotal)*100)
+			fmt.Printf("%.1f id, ", float32(cpuIdle)/float32(cpuTotal)*100)
+			fmt.Printf("%.1f wa, ", float32(cpuIOWait)/float32(cpuTotal)*100)
+			fmt.Printf("%.1f hi, ", float32(cpuIRQ)/float32(cpuTotal)*100)
+			fmt.Printf("%.1f si, ", float32(cpuSoftIRQ)/float32(cpuTotal)*100)
+			fmt.Printf("%.1f st\n", float32(cpuSteal)/float32(cpuTotal)*100)
+
+		} else {
+			break
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
